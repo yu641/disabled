17987121819bc7dc37584249d730717c50c8d88d
yu641 <blindthewind@gmail.com>
2024-12-17 02:08:58 +0900

    Update 2_subpage_2.py

--- 2_subpage_2.py
+++ 2_subpage_2.py
@@ -20,12 +20,12 @@ dp=df[['총장애인']] #장애인 인구수
 dp_rate=df[['장애인구비율']] #장애인구비율=장애인인구수/총인구수*100
 
 ###============장애인의 교육수준============###
-st.title('## 장애인 인구 유형별 분포')
+st.title('# 장애인 인구 유형별 분포')
 col1, col2 = st.columns([1,1])
 
 # 장애인의 학력 파이차트(전체)
 with col1:
-    st.write('## 장애인의 학력')
+    st.write('장애인의 학력')
     fig=px.pie(
         values = [8.5, 26.8, 16.3, 31.0, 17.4],
         names = ['무학','초등학교','중학교','고등학교','대학이상']
@@ -35,10 +35,11 @@ with col1:
         textinfo = 'label+percent'
     )
     fig.show()
+    st.plotly_chart(fig)
 
 # 장애인의 학력 파이차트(25-65세)
 with col1:
-    st.write('## 장애인의 학력')
+    st.write('장애인의 학력')
     fig=px.pie(
         values = [1.7, 9.5, 12.2, 47.5, 29.0],
         names = ['무학','초등학교','중학교','고등학교','대학이상']
@@ -48,13 +49,14 @@ with col1:
         textinfo = 'label+percent'
     )
     fig.show()
+    st.plotly_chart(fig)
 
 st.write('')
 
 col1, col2, col3 = st.columns([1,1,1])
 # 시도별 특수학교 수 바차트
 with col1:
-    st.write('## 특수학교 수')
+    st.write('특수학교 수')
     fig = px.bar(
         data_frame = df4,
         x='특수학교수',
@@ -63,10 +65,11 @@ with col1:
         orientation = 'h'
         )
     fig.show()
+    st.plotly_chart(fig)
 
 # 시도별 특수학교 학생 수 바차트
 with col2:
-    st.write('## 특수학교 학생 수')
+    st.write('특수학교 학생 수')
     fig = px.bar(
         data_frame = df4,
         x='학생수',
@@ -75,10 +78,11 @@ with col2:
         orientation = 'h'
         )
     fig.show()
+    st.plotly_chart(fig)
 
 # 시도별 특수학교 교원 수 바차트
 with col3:
-    st.write('## 특수학교 교원 수')
+    st.write('특수학교 교원 수')
     fig = px.bar(
         data_frame = df4,
         x='교원수',
@@ -88,14 +92,15 @@ with col3:
         )
     fig.show()
 st.write('')
+st.plotly_chart(fig)
 
 ###============장애인의 경제활동 현황============###
-st.title('## 장애인 경제활동 현황')
+st.title('# 장애인 경제활동 현황')
 tab_1, tab_2, tab_3 = st.tabs(['개요','지역','교육수준'])
 
 # 장애인 경제활동인구 파이차트
 with tab_1:
-    st.write('## 장애인 경제활동 인구')
+    st.write('장애인 경제활동 인구')
     fig=px.pie(
         values = [880929,35653,1672465],
         names = ['취업자','실업자','비경제활동인구']
@@ -105,12 +110,13 @@ with tab_1:
         textinfo = 'label+value+percent'
     )
     fig.show()
+    st.plotly_chart(fig)
 
 ## 지역별
 
 with tab_2:
-    st.write('## 지역별 경제활동 현황')
-    st.write('# 지역별 경제활동 인구 수')
+    st.write('# 지역별 경제활동 현황')
+    st.write('지역별 경제활동 인구 수')
     fig = px.bar(
         data_frame = df5,
         x=['취업자', '실업자', '비경제활동인구'],
@@ -118,31 +124,35 @@ with tab_2:
         orientation = 'h'
         ) # 지역별 경제활동상태 바차트
     fig.show()
+    st.plotly_chart(fig)
 
-    st.write('# 지역별 경제활동 인구 비율')
+    st.write('지역별 경제활동 인구 비율')
     fig = px.bar(
         data_frame = df5,
         x='지역별',
         y='경활률 (%)',
         ) # 지역별 경제활동인구비율
-    fig.show() 
+    fig.show()
+    st.plotly_chart(fig)
 
 ## 교육수준별
 with tab_3:
-    st.write('## 교육수준별 경제활동 현황')
-    st.write('# 교육수준별 경제활동 인구 수')
+    st.write('#교육수준별 경제활동 현황')
+    st.write('교육수준별 경제활동 인구 수')
     ig = px.bar(
         data_frame = df6,
         x=['취업자', '실업자', '비경제활동인구'],
         y='교육정도별',
         orientation = 'h'
         ) # 교육수준별 경제활동상태 바차트
-    fig.show() 
+    fig.show()
+    st.plotly_chart(fig)
 
-    st.write('# 교육수준별 경제활동 인구 비율')
+    st.write('교육수준별 경제활동 인구 비율')
     fig = px.bar(
         data_frame = df6,
         x='교육정도별',
         y='경활률 (%)',
-        )
-    fig.show() # 교육수준별 경제활동인구비율
+        ) # 교육수준별 경제활동인구비율
+    fig.show()
+    st.plotly_chart(fig)
