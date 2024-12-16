e886b88dccb68d73e76c034516ad644a7bfd51b1
yu641 <blindthewind@gmail.com>
2024-12-17 03:25:36 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ gdf_dp=pd.concat([gdf_korea_sido, dp], axis=1) #시군구별 장애인 인구 �
 ax = gdf_dp.plot(column='총장애인', legend=True, cmap="Reds", k=7)
 ax.set_axis_off()
 plt.show()
-st.geopandas_Chart(ax)
+st.matplotlib_Chart(ax)
 st.title('')
 
 st.header('시군구별 장애인 비율 [단위: %]')
